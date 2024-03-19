108fc5ffc5a71b0eae89881862d2082c8da32662
Mathis <mathismoreau0610@gmail.com>
2024-03-19 22:43:58 +0100

    add header of home details page

--- src/app/main/components/home/home-details/home-details/home-details.component.ts
+++ src/app/main/components/home/home-details/home-details/home-details.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { itemsInfo } from '~/app/shared/const/item-info/items-info.const';
 import { ItemInfo } from '~/app/shared/types/items/item-info.type';
 
@@ -11,10 +11,17 @@ import { ItemInfo } from '~/app/shared/types/items/item-info.type';
 export class HomeDetailsComponent {
   item: ItemInfo | null;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.item =
       itemsInfo.find(
         item => item.city === this.route.snapshot.params['city']
       ) ?? null;
+  }
+
+  onClickBackArrow(): void {
+    void this.router.navigate(['/home']);
   }
 }
