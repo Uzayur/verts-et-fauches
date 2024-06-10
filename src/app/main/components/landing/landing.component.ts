8d38d80df0151928a86c02d2b9c1f54b76b6a6e6
Mathis <mathismoreau0610@gmail.com>
2024-06-10 14:00:35 +0200

    add planned and finished travel in profile

--- src/app/main/components/landing/landing.component.ts
+++ src/app/main/components/landing/landing.component.ts
@@ -16,7 +16,7 @@ export class LandingComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.isShow = false;
-      void this.router.navigate(['/home']);
+      void this.router.navigate(['/profile/travels']);
     }, this.timeout);
   }
 }
