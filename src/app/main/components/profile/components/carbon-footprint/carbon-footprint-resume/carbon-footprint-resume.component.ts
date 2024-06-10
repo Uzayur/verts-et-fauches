6dcf2ecf9f8c29687637de389b8ed75e641f87c1
Mathis <mathismoreau0610@gmail.com>
2024-06-10 18:32:29 +0200

    add carbon footprint travel resume

--- src/app/main/components/profile/components/carbon-footprint/carbon-footprint-resume/carbon-footprint-resume.component.ts
+++ src/app/main/components/profile/components/carbon-footprint/carbon-footprint-resume/carbon-footprint-resume.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { carbonFootprintProgram } from '~/app/shared/const/travel/carbon-footprint-program.const';
 
 @Component({
   selector: 'app-carbon-footprint-resume',
   templateUrl: './carbon-footprint-resume.component.html',
-  styleUrl: './carbon-footprint-resume.component.css'
+  styleUrl: './carbon-footprint-resume.component.css',
 })
 export class CarbonFootprintResumeComponent {
-
+  protected readonly carbonFootprintItem = carbonFootprintProgram;
 }
